cd97af4716aa3fc8d9f84c31024642473fcb4809
yanqi <yanqi@itiger.com>
2023-03-17 20:41:26 +0800

    feat(utils): init openai

---
+++ src/openai/chat.ts
@@ -0,0 +1,25 @@
+import { Configuration, OpenAIApi } from "openai";
+import { env } from "process";
+
+
+const fineTunedModel = 'curie:ft-dev-2023-03-17-08-29-16'; // curie
+
+const configuration = new Configuration({
+    apiKey: env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+async function main() {
+    const completion = await openai.createChatCompletion({
+        model: fineTunedModel || 'gpt-3.5-turbo',
+        messages: [
+            {
+                role: 'user',
+                content: '哪些模型支持 createChatCompletion api'
+            }
+        ]
+    });
+    console.log(completion.data.choices[0]);
+}
+
+main();
